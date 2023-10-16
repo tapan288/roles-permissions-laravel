756380b018a4a5cb9e01eda988cd1ae767cf76e1
tapan288 <tapansharma69@gmail.com>
2023-10-16 22:02:25 +0545

    implement authorization checks on the frontend

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -13,10 +13,12 @@
                     </div>
 
                     <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                        <a href="{{ route('todos.create') }}" as="button"
-                            class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
-                            Add Todo
-                        </a>
+                        @can('todo_create')
+                            <a href="{{ route('todos.create') }}" as="button"
+                                class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                                Add Todo
+                            </a>
+                        @endcan
                     </div>
                 </div>
 
